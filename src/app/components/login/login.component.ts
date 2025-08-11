812edcbe62dc39a936ead2c9b41c637fd82e62a2
Matheusxr77 <suehtamxr77@gmail.com>
2025-08-11 11:19:48 -0300

    feat(auth): implement login service and component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, RouterModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  login() {
+    this.error = '';
+    this.auth.login(this.username, this.password).subscribe({
+      next: (res) => {
+        if (res && res.token) {
+          localStorage.setItem('token', res.token);
+        }
+        this.router.navigate(['/tasks']);
+      },
+      error: (err) => {
+        this.error = err.error?.message || 'Usuário ou senha inválidos.';
+      }
+    });
+  }
+}
